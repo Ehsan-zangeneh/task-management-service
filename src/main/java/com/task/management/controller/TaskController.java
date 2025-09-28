533d4042ef09429edad02d097f85320201f54c93
Ehsan <ehsan.zangneh@gmail.com>
2025-09-28 22:11:46 +0200

    just delete operation needs to be implemented

--- src/main/java/com/task/management/controller/TaskController.java
+++ src/main/java/com/task/management/controller/TaskController.java
@@ -10,12 +10,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.util.UUID;
 
+@RestController
 @RequiredArgsConstructor
 @Slf4j
 public class TaskController implements TaskManagementApi {
